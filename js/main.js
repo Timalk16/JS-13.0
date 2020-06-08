316dfe909290914ce7266bcc7970a9c3237112f7
Timofey Malakanchikov <tima160604@mail.ru>
2020-06-08 16:19:27 +0300

    refact: refact

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
 'use srtict';
 
 //Задание 1
-let money = prompt('Ваш месячный доход?');
-let income = 'фриланс';
-let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-let deposit = confirm('Есть ли у вас депозит в банке?');
-let mission = 500000;
-let period = 5;
-let expenses1 = prompt('Введите обязательную статью расходов?');
-let expenses2 = prompt('Введите обязательную статью расходов?');
-let amount1 = prompt('Во сколько это обойдется?');
-let amount2 = prompt('Во сколько это обойдется?');
-let moneyNumber = Number.parseInt(money);
-let amountNumber1 = Number.parseInt(amount1);
-let amountNumber2 = Number.parseInt(amount2);
-let amountNumberAll = amountNumber1 + amountNumber2;
-let budgetMonth = moneyNumber - amountNumberAll;
-let missionCompleted = Math.ceil((mission / budgetMonth));
+let money = +prompt('Ваш месячный доход?'),
+    income = 'фриланс',
+    addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую'),
+    deposit = confirm('Есть ли у вас депозит в банке?'),
+    mission = 500000,
+    period = 5,
+    expenses1 = prompt('Введите обязательную статью расходов?'),
+    amount1 = +prompt('Во сколько это обойдется?'),
+    expenses2 = prompt('Введите обязательную статью расходов?'),
+    amount2 = +prompt('Во сколько это обойдется?'),
+    amountNumberAll = amountNumber1 + amountNumber2,
+    budgetMonth = moneyNumber - amountNumberAll,
+    missionCompleted = Math.ceil((mission / budgetMonth));
 
 
 
